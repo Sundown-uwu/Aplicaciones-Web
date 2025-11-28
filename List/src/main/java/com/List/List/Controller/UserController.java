8345a4a33eee9bb7570494b923dc8a47daabf815
ulisescam <20243ds023@utz.edu.mx>
2025-11-28 11:56:52 -0600

    Springboot jeringas WIP

---
+++ List/src/main/java/com/List/List/Controller/UserController.java
@@ -0,0 +1,52 @@
+package com.List.List.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+public class UserController {
+    @RestController
+    @RequestMapping("/users")
+    public class UserController{
+        private final UserService service;
+
+        public UserController(UserService service){
+            this.service = service;
+        }
+
+        @GetMapping("/{id}")
+        public ResponseEntity<Object> getById(id)
+        .map(u -> ResponseEntity.ok ((Object) u))
+        .orElse(
+            ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(Map.of("error", "Usuario no encontrado"))
+        );
+    }
+    @GetMapping
+    public List<User> list(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size,
+        @RequestParam(required = false) String search
+    ){
+        return service.findAll(page, size, search);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody @valid CreatureUserDTO dto) {
+        User created = service.save(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> update(@PathVariable Long id, @RequestBody @Valid CreateUserDTO dto) {
+        return service.update(id, dto)
+        .map(u -> ResponseEntity.ok((Object) u))
+        .orElse(
+            ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(Map.of("error", "Usuario no encontrado"))
+        );
+    }
+
+}
